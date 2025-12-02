e4847160ea19f57cc2cd805bc498e60c238ec715
kostya222-b <kostyabryazgun@gmail.com>
2025-12-02 09:53:47 +0300

    first commit

--- src/quest_ans.py
+++ src/quest_ans.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI, Request, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 import httpx
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
@@ -19,8 +23,11 @@ app.add_middleware(
 @app.post('/api/proxy-yandex-gpt')
 async def proxy_yandex_gpt(request: Request):
     try:
+        logger.info(f"Headers: {dict(request.headers)}")  # Логируем все заголовки
         body = await request.json()
-        authorization_header = request.headers.get("X-Authorization", "aje7d9vbut2at538rm45")  # Получаем API ключ из заголовка
+        authorization_header = request.headers.get("x-authorization", "aje7d9vbut2at538rm45")
+        logger.info(f"Authorization header: {authorization_header}")  # Логируем заголовок авторизации
+
         headers = {
             "Content-Type": "application/json",
             "Authorization": f"Api-Key {authorization_header}"
@@ -34,6 +41,8 @@ async def proxy_yandex_gpt(request: Request):
             response.raise_for_status()
             return response.json()
     except httpx.HTTPStatusError as e:
+        logger.error(f"HTTP error: {e.response.status_code}, {e.response.text}")
         raise HTTPException(status_code=e.response.status_code, detail=f"Ошибка Yandex GPT: {e.response.text}")
     except Exception as e:
+        logger.error(f"Server error: {str(e)}")
         raise HTTPException(status_code=500, detail=f"Ошибка сервера: {str(e)}")
